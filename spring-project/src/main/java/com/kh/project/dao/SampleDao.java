221ad036e28dcca79c07ce1021a8f9da9456ab6a
kho96 <k____ho__@naver.com>
2022-12-31 05:19:29 +0900

    프로젝트 추가(sample java파일, test파일)

---
+++ spring-project/src/main/java/com/kh/project/dao/SampleDao.java
@@ -0,0 +1,8 @@
+package com.kh.project.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SampleDao {
+
+}
---
+++ spring-project/src/main/java/com/kh/project/service/SampleService.java
@@ -0,0 +1,8 @@
+package com.kh.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+}
