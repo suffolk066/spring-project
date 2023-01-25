11279ca6b5ab8b17742301ef9d6a95c37fa0d4db
kho96 <k____ho__@naver.com>
2023-01-25 12:35:45 +0900

    현재까지 mapper 작업 commit

--- spring-project/src/main/java/com/kh/project/service/CinemaService.java
+++ spring-project/src/main/java/com/kh/project/service/CinemaService.java
@@ -1,21 +1,28 @@
 package com.kh.project.service;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.kh.project.dao.AdminMovieDao;
 import com.kh.project.dao.CinemaDao;
 import com.kh.project.dao.SampleDao;
 import com.kh.project.vo.CinemaVo;
 import com.kh.project.vo.SampleVo;
+import com.kh.project.vo.SeatDto;
 
 @Service
 public class CinemaService {
 	
 	@Autowired
 	private CinemaDao dao;
+	
+	@Autowired
+	private AdminMovieDao adminDao;
 	
 	public List<String> getMovieList() {
 		return dao.getMovieList();
@@ -33,27 +40,44 @@ public class CinemaService {
 		return dao.getDateList();
 	}
 	
-	public List<String> getCinemaList(String cinema_movie) {
+	public List<String> getCinemaList(int cinema_movie) {
 		return dao.getCinemaList(cinema_movie);
 	}
 
-	public List<Date> getDateList(String cinema_name, String cinema_movie) {
-		return dao.getDateList(cinema_name, cinema_movie);
+	public List<Timestamp> getDateList(String cinema_name, int cinema_movie_no) {
+		System.out.println("timestamp : " + dao.getDateList(cinema_name, cinema_movie_no));
+		return dao.getDateList(cinema_name, cinema_movie_no);
 	}
 
-	public List<Date> getTimeList(String cinema_name, String cinema_movie, String day) {
-		return dao.getTimeList(cinema_name, cinema_movie, day);
+	public List<Date> getTimeList(String cinema_name, int cinema_movie_no, String day) {
+		return dao.getTimeList(cinema_name, cinema_movie_no, day);
 	}
 
-	public int getSeatCol(String cinema_name, String cinema_movie, String cinema_time) {
-		return dao.getSeatCol(cinema_name, cinema_movie, cinema_time);
+	public int getSeatCol(String cinema_name, int cinema_movie_no, String cinema_time) {
+		return dao.getSeatCol(cinema_name, cinema_movie_no, cinema_time);
 	}
 
-	public List<CinemaVo> getSeatAll(String cinema_name, String cinema_movie, String cinema_time) {
-		return dao.getSeatAll(cinema_name, cinema_movie, cinema_time);
+	public List<CinemaVo> getSeatAll(String cinema_name, int cinema_movie_no, String cinema_time) {
+		return dao.getSeatAll(cinema_name, cinema_movie_no, cinema_time);
 	}
 
-	public int getSeatsLeft(String cinema_name, String cinema_movie, String cinema_time) {
-		return dao.getSeatsLeft(cinema_name, cinema_movie, cinema_time);
+	public int getSeatsLeft(String cinema_name, int cinema_movie_no, String cinema_time) {
+		return dao.getSeatsLeft(cinema_name, cinema_movie_no, cinema_time);
+	}
+
+	@Transactional
+	public boolean addCinema(CinemaVo vo, List<SeatDto> list) {
+		System.out.println("list : " + list);
+		System.out.println();
+		System.out.println("vo.getCinema_name() : " + vo.getCinema_name());
+		boolean result = adminDao.insertCinemaName(vo.getCinema_name());
+		boolean result2 = false;
+		for(SeatDto dto : list) {
+			result2 = dao.addCinema(vo, dto);
+		}
+		if (result && result2) {
+			return true;
+		}
+		return false;
 	}
 }
